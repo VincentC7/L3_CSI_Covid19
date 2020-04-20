63dc4bc399fbfe10148fb2ba4414ef639b936dba
alain <alain.cossi1n@gmail.com>
2020-04-20 14:13:29 +0200

    fsd
    dfs

--- src/Controller/StatistiquesController.php
+++ src/Controller/StatistiquesController.php
@@ -7,6 +7,8 @@ namespace L3_CSI_Covid19\Controller;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
+use L3_CSI_Covid19\Model\Patient;
+
 class StatistiquesController extends Controller {
 
     /**
@@ -17,11 +19,14 @@ class StatistiquesController extends Controller {
         $this->render($response,'pages/stats.twig');
     }
 
-    public function global_stat(RequestInterface $request, ResponseInterface $response, $cutTime){
-        /*
-          cutTime sert a découpé les échelles de temps
-        */
+    public function global_stat(RequestInterface $request, ResponseInterface $response){
+        $dataStat = new Array();
 
-
+        //tranche de 10
+        for($i = 0; $i < 10; $i++){
+            $dataStat[$i][0] = "M";
+            $dataStat[$i][1] = "F";
+            $dataStat[$i][0][0] = Patient
+        }
     }
 }
